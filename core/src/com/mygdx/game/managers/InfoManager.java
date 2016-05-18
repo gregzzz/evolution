2c5f1192774314524ee6577004384674d5313f23
gregzzz <tobiaszworonko@gmail.com>
2016-05-18 14:46:59 +0200

    czytanie opisow kart z pliku

--- core/src/com/mygdx/game/managers/InfoManager.java
+++ core/src/com/mygdx/game/managers/InfoManager.java
@@ -4,10 +4,7 @@ package com.mygdx.game.managers;
 
 import components.enums.Card;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -17,27 +14,55 @@ public class InfoManager {
 
     public InfoManager(){
         descriptions = new HashMap<Card,String>();
-        descriptions.put(Card.AQUATIC,"This animal can only attack and be attacked by animals with this perk.");
-        descriptions.put(Card.CAMOUFLAGE,"Only an animal with the Sharp Sight perk can attack this animal.");
-        descriptions.put(Card.COMMUNICATION,"Both animals with this perk eat at the same time.");
-        descriptions.put(Card.COOPERATIONC,"If one of these animals recieves a food token, the other recieves one for free.");
-        descriptions.put(Card.COOPERATIONF,"If one of these animals recieves a food token, the other recieves one for free.");
-        descriptions.put(Card.HIBERNATION,"Once every two turns this animal may be considered fed without the need to eat.");
-        descriptions.put(Card.MASSIVEC,"This animal can only be attacked by animals with this perk.It also needs an additional food token.");
-        descriptions.put(Card.MASSIVEF,"This animal can only be attacked by animals with this perk.It also needs an additional food token.");
-        descriptions.put(Card.MIMICRY,"If this animal is attacked, the player may choose another viable target to be attacked in its stead.");
-        descriptions.put(Card.PARASITEC,"Adds 2 to the number of food needed. Can only be attached to enemy animals.");
-        descriptions.put(Card.PARASITEF,"Adds 2 to the number of food needed. Can only be attached to enemy animals.");
-        descriptions.put(Card.PASTURAGE,"This animal may substract one food token from the pool each turn.");
-        descriptions.put(Card.PIRACY,"This animal can steal one food token from an animal not yet fed each turn.");
-        descriptions.put(Card.ROAR,"This animal cannot be attacked once it is fed.");
-        descriptions.put(Card.SCAVENGER,"Whenever an animal is eaten, this animal recieves an additional food token");
-        descriptions.put(Card.SHARPSIGHT,"An animal with this perk can attack animals with the Camouflage perk.");
-        descriptions.put(Card.SPEED,"This animal has a 50% chance of escaping each attacker.");
-        descriptions.put(Card.SYMBIOSIS,"The protected animal may only be attacked if the other one is dead, however it can only eat once the other one is fed.");
-        descriptions.put(Card.TAILTOSS,"When this animal is attacked, it can discard one perk instead of being eaten.The attacker recieves one food token.");
-        descriptions.put(Card.TOXIC,"An animal that has eaten this animal will die at the end of this feeding phase.");
-
+        try {
+            File file = new File("core/assets/info.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            line = bufferedReader.readLine();
+            descriptions.put(Card.AQUATIC, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.CAMOUFLAGE, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.COMMUNICATION, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.COOPERATIONC, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.COOPERATIONF, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.HIBERNATION, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.MASSIVEC, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.MASSIVEF, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.MIMICRY, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.PARASITEC, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.PARASITEF, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.PASTURAGE, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.PIRACY, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.ROAR, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.SCAVENGER, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.SHARPSIGHT, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.SPEED, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.SYMBIOSIS, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.TAILTOSS, line);
+            line = bufferedReader.readLine();
+            descriptions.put(Card.TOXIC, line);
+            fileReader.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getDescription(Card cardName){
